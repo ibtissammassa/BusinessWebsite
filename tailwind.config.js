fcbe3a9fd5d3721bcba4be6e410a253d597565f4
ibtissammassa <ibtissammassa16@gmail.com>
2023-02-08 23:39:09 +0100

    header styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./public/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        Nunito:['Nunito'],
+      },
+    },
   },
   plugins: [],
 }
