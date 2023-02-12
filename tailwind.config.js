92d664aa408d1e0cb89ecc7384b36409ac2e2ac5
ibtissammassa <ibtissammassa16@gmail.com>
2023-02-12 17:18:24 +0100

    main section

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,10 +14,19 @@ module.exports = {
       height: {
         '17': '4.5rem',
         '90':'22rem',
+        '100':'38rem',
       },
       screens: {
       'mdl': '991px',
       // => @media (min-width: 991px) { ... }
+      },
+      keyframes:{
+        toRight :{
+          '0%,100%':{transform:translateX(0)},
+        }
+      },
+      animation:{
+        toRight:'toRight'
       }
     }
   },
