72d08c30808a4038a5c588140dd373a51c1355b0
ibtissammassa <ibtissammassa16@gmail.com>
2023-02-13 23:58:14 +0100

    main section responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,13 +14,16 @@ module.exports = {
       height: {
         '17': '4.5rem',
         '90':'22rem',
+        '98':'35rem',
         '100':'38rem',
         '200':'50rem',
         '300':'60rem',
       },
       screens: {
-      'mdl': '991px',
-      // => @media (min-width: 991px) { ... }
+      'mdl': '991px',// => @media (min-width: 991px) { ... }
+      'mdd':'856px',// => @media (min-width: 856px) { ... }
+      'l':'1133px',// => @media (min-width: 1133px) { ... }
+
       },
       backgroundImage: {
         'cloud': "url('img/cloud.png')",
